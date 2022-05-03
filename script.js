4c6e485cf0b3e83e50bb222cebee8a394600b7b7
sish <100435804+sishxd@users.noreply.github.com>
2022-05-03 12:12:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {new TypeIt("#type", {strings: ["I'm an Indian Web Developer who uses MERN Stack and ;)"],}).go();});
+document.addEventListener("DOMContentLoaded", function () {new TypeIt("#type", {strings: ["I'm an Indian Web and App Developer ;)"],}).go();});
 
 
 
